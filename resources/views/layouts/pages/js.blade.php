87cccaa0c036bc382676659e0b809c54b31e9939
PhuongDuy1034 <0306191307@caothang.edu.vn>
2023-06-01 21:29:44 +0700

    update Login admin and login user

---
+++ resources/views/layouts/pages/js.blade.php
@@ -0,0 +1,17 @@
+<script src="{{asset('frontend/js/vendor/jquery-library.js')}}"></script>
+<script src="{{asset('frontend/js/vendor/bootstrap.min.js')}}"></script>
+<script src="https://maps.google.com/maps/api/js?key=AIzaSyCR-KEWAVCn52mSdeVeTqZjtqbmVJyfSus&amp;language=en"></script>
+<script src="{{asset('frontend/js/owl.carousel.min.js')}}"></script>
+<script src="{{asset('frontend/js/jquery.vide.min.js')}}"></script>
+<script src="{{asset('frontend/js/countdown.js')}}"></script>
+<script src="{{asset('frontend/js/jquery-ui.js')}}"></script>
+<script src="{{asset('frontend/js/parallax.js')}}"></script>
+<script src="{{asset('frontend/js/countTo.js')}}"></script>
+<script src="{{asset('frontend/js/appear.js')}}"></script>
+<script src="{{asset('frontend/js/gmap3.js')}}"></script>
+<script src="{{asset('frontend/js/main.js')}}"></script>
+
+<!--************************************
+        js auth
+*************************************-->
+<script src="{{ asset('js/app.js') }}" defer></script>
